5cde934c801de375edb81b93dc1492edb398ffa9
Dennis Chen <c.w.dennis@gmail.com>
2013-08-20 11:27:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Tcca::Application.config.secret_key_base = '5acdd8201c29cc445684da1b5321886c04d89deab87e00a4613f5612f59a9f14feb03b0378e5ac6da438499d194a0bb720ecb0bae03c33046703604eb3bc128b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tcca::Application.config.session_store :cookie_store, key: '_tcca_session'
