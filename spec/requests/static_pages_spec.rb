471c6a755892bb7fb2d3ff9ed6077583d63d7338
Dennis Chen <c.w.dennis@gmail.com>
2013-08-21 22:30:00 -0700

    Added guard and spork for faster and more efficient rspec unit testing

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'TCCA'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('TCCA')
+    end
+    it "should have the title 'TCCA'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("TCCA")
+    end
+  end
+end
